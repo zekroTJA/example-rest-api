60e4117dae1da24e1a6a688da0538ee450ecfc62
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2024-05-07 15:12:44 +0200

    update redis client

--- lib/persistence/redis.rs
+++ lib/persistence/redis.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use redis::{AsyncCommands, Client};
+use redis::{Client, Commands};
 
 use super::Persistence;
 
@@ -19,28 +19,28 @@ impl RedisClient {
 #[async_trait]
 impl Persistence for RedisClient {
     async fn set(&self, key: &str, val: &[u8], expiry: usize) -> Result<()> {
-        let mut con = self.client.get_async_connection().await?;
-        con.set_ex(key, val, expiry).await?;
+        let mut con = self.client.get_connection()?;
+        con.set_ex(key, val, expiry as u64)?;
         Ok(())
     }
 
     async fn get(&self, key: &str) -> Result<Option<Vec<u8>>> {
-        let mut con = self.client.get_async_connection().await?;
-        Ok(con.get(key).await?)
+        let mut con = self.client.get_connection()?;
+        Ok(con.get(key)?)
     }
 
     async fn exists(&self, key: &str) -> Result<bool> {
-        let mut con = self.client.get_async_connection().await?;
-        Ok(con.exists(key).await?)
+        let mut con = self.client.get_connection()?;
+        Ok(con.exists(key)?)
     }
 
     async fn list(&self, key: &str) -> Result<Vec<(String, Vec<u8>)>> {
-        let mut con = self.client.get_async_connection().await?;
-        let keys: Vec<String> = con.keys(key).await?;
+        let mut con = self.client.get_connection()?;
+        let keys: Vec<String> = con.keys(key)?;
 
         let mut res = Vec::with_capacity(keys.len());
         for k in keys {
-            let val: Option<Vec<u8>> = con.get(&k).await?;
+            let val: Option<Vec<u8>> = con.get(&k)?;
             if let Some(val) = val {
                 res.push((k, val));
             }
@@ -50,8 +50,8 @@ impl Persistence for RedisClient {
     }
 
     async fn delete(&self, keys: &[&str]) -> Result<()> {
-        let mut con = self.client.get_async_connection().await?;
-        con.del(keys).await?;
+        let mut con = self.client.get_connection()?;
+        con.del(keys)?;
         Ok(())
     }
 }
